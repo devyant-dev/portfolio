141b0c1203af45324b64788913952abc0e53a84e
devyant <devvyves@gmail.com>
2023-04-05 15:15:03 +0900

    0405

--- pages/work.js
+++ pages/work.js
@@ -1,10 +1,91 @@
 import React from 'react';
 import style from '@/styles/Work.module.scss';
+import data from '../data/work.json';
 
 const work = () => {
+
   return (
     <div className='page_container'>
-      work
+      <div className={style.work_container}>
+        {/* <div className={style.work_wrapper}>
+
+          <div className={style.work_info_wrap}>
+            <p className={style.info_num}>
+              01
+            </p>
+            <p className={style.info_title}>
+              Clone Coding
+              Project
+            </p>
+            <p className={style.info_project}>
+              personal project
+            </p>
+
+            <p className={style.info_cate}>
+              1st Project <br />
+              HTML5 / CSS3 / JavaScript
+            </p>
+          </div>
+
+          <div className={style.work_sub_wrap}>
+            <div className={style.sub_cate_wrap}>
+              <span className={style.sub_cate}>
+                responsive web
+              </span>
+            </div>
+
+            <div className={style.sub_icon_wrap}>
+              <div className={style.plus_row}></div>
+              <div className={style.plus_column}></div>
+            </div>
+
+          </div>
+
+        </div> */}
+
+        {
+          data.workdata.map( (obj, idx) =>   (<div key={idx} className={style.work_wrapper}>
+          <div className={style.work_info_wrap}>
+            <p className={style.info_num}>
+              0{obj.number}
+            </p>
+            <p className={style.info_title}>
+              {obj.title}
+            </p>
+            <p className={style.info_project}>
+              {obj.info}
+            </p>
+
+            <p className={style.info_cate}>
+              {obj.count} <br />
+              {obj.cate}
+            </p>
+          </div>
+
+          <div className={style.work_sub_wrap}>
+            <div className={style.sub_cate_wrap}>
+              <span className={style.sub_cate}>
+              {obj.type}
+              </span>
+            </div>
+
+            <div className={style.sub_icon_wrap}>
+              <div className={style.plus_row}></div>
+              <div className={style.plus_column}></div>
+            </div>
+
+          </div>
+
+        </div>
+            )
+          )
+        }
+
+        {
+          data.workdata.length%2 == 0 ? '' : <div className={style.empty_wrapper}></div>
+        }
+
+      </div>
     </div>
   );
 };
